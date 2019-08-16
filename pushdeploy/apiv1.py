9cb44e5622a65db2fd33819f016a04abf6cb40eb
mdgreenwald <mdgreenwald@gmail.com>
2019-08-16 04:14:32 -0400

    matthew: parameterized funcs
    Signed-off-by: mdgreenwald <mdgreenwald@gmail.com>

--- pushdeploy/apiv1.py
+++ pushdeploy/apiv1.py
@@ -26,26 +26,36 @@ def init_api():
     and stores them in global"""
     g.configuration = config.load_incluster_config()
     g.api_instance = client.AppsV1Api(client.ApiClient(g.configuration))
-    # g.api_instance = kubernetes.client.AppsV1Api(kubernetes.client.ApiClient(g.configuration))
+    g.PD_NAMESPACE = current_app.config['PD_NAMESPACE']
+    g.PD_DEPLOYMENT = current_app.config['PD_DEPLOYMENT']
+    g.PD_REGISTRY = current_app.config['PD_REGISTRY']
 
 def read_deployment():
+    namespace = "%s" % str(g.PD_NAMESPACE)
+    field = "metadata.name=%s" % str(g.PD_DEPLOYMENT)
     api_response = g.api_instance.list_namespaced_deployment(
-        namespace="default",
-        field_selector="metadata.name=http-svc"
+        namespace=namespace,
+        field_selector=field
     )
     if len(api_response.items) == 1:
         return api_response.items[0]
     else:
-        return "Deployment selectors not unique enough."
+        return "Deployment selector not unique enough."
 
-def update_deployment(deployment):
-    deployment.spec.template.spec.containers[0].image = "gcr.io/google_containers/echoserver:1.10"
+def update_deployment(deployment, image_name, image_tag):
+    image = "%s/%s:%s" % (g.PD_REGISTRY, image_name, image_tag)
+    deployment.spec.template.spec.containers[0].image = image
     api_response = g.api_instance.patch_namespaced_deployment(
-        name="http-svc",
-        namespace="default",
+        name=g.PD_DEPLOYMENT,
+        namespace=g.PD_NAMESPACE,
         body=deployment,
         field_manager="push-deploy")
     print("Deployment updated. status='%s'" % str(api_response.status))
+
+@bp.route('/', methods=['GET'])
+@jwt_required
+def index():
+    return jsonify(), 200
 
 @bp.route('/auth', methods=['POST'])
 def login():
@@ -63,16 +73,13 @@ def login():
         return jsonify({"msg": "Bad username or password"}), 401
 
     # Identity can be any data that is json serializable
-    access_token = create_access_token(identity=username, expires_delta=timedelta(seconds=300))
+    access_token = create_access_token(identity=username, expires_delta=timedelta(seconds=90))
     return jsonify(access_token=access_token), 200
 
-@bp.route('/', methods=['GET'])
-@jwt_required
-def index():
-    return jsonify(), 200
-    
 @bp.route('/deploy', methods=['GET'])
 @jwt_required
 def deploy():
-    deploy = update_deployment(deployment=read_deployment())
-    return jsonify(msg=deploy), 200
+    commit_sha = request.args['commit_sha']
+    image_name = request.args['image_name']
+    deploy = update_deployment(deployment=read_deployment(), image_name=image_name, image_tag=commit_sha)
+    return jsonify(msg=deploy), 201
